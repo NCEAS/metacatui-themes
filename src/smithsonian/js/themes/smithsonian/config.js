49cac689dd8d21481244c6db8e6ad15d904bb2b8
Ian Nesbitt <ian.nesbitt@gmail.com>
2024-05-06 06:36:23 -0700

    adding search map formatting (#29)

--- src/smithsonian/js/themes/smithsonian/config.js
+++ src/smithsonian/js/themes/smithsonian/config.js
@@ -8,6 +8,7 @@ MetacatUI.AppConfig = Object.assign({
     baseUrl: "https://smithsonian.dataone.org/",
     emailContact: "support@dataone.org",
 
+    /* Map */
     useDeprecatedDataCatalogView: false,
     enableCesium: true,
     // temporary token restricted to smithsonian, for testing
@@ -15,6 +16,176 @@ MetacatUI.AppConfig = Object.assign({
     mapKey: "AIzaSyDGzLLaG3iiTUA8CQ1hLvgmekQOsEZ94ac",
     googleAnalyticsKey: "G-Z4Z285RHKC",
 
+    catalogSearchMapOptions: {
+        "homePosition": {
+          "longitude": -79.54379128281369,
+          "latitude": 8.962437979242821,
+          "height": 1000000,
+          "heading": 0,
+          "pitch": -90,
+          "roll": 0
+        },
+        "layerCategories": [
+          {
+            "label": "LiDAR",
+            "icon": "<svg height='800px' width='800px' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' aria-hidden='true' role='img' class='iconify iconify--gis' preserveAspectRatio='xMidYMid meet'><path d='M100 70l-4.242-1.763h-.001L63.18 54.69l.002-.001l-3.368-1.4l-.003.001L49.497 49L0 69.999L50.716 100zm-7.232.002l-6.876 4.057l-9.086-4.058l6.936-3.836zm-10.123 5.973l-7.484 4.418L66 75.975l7.532-4.165zM71.62 82.483l-8.177 4.825l-9.21-4.826l8.21-4.538zm-12.054 7.114l-8.972 5.295l-9.225-5.296l8.979-4.965zm20.249-25.1l-6.958 3.741l-8.377-3.742l6.995-3.544zM69.576 70l-7.548 4.058L53.613 70l7.572-3.834zm-11.113 5.974l-8.216 4.417l-8.427-4.418l8.222-4.165zM46.36 82.482l-8.977 4.826l-8.409-4.826l8.96-4.54zm21.48-23.074l-7.007 3.46l-7.747-3.46l7.027-3.284zm-10.306 5.088l-7.577 3.74l-7.758-3.74l7.583-3.544zM46.384 70l-8.22 4.058L30.42 70l8.208-3.836zm-12.102 5.974l-8.948 4.418l-7.695-4.418l8.911-4.165zm22.46-21.284l-7.033 3.21l-7.185-3.21l7.035-3.051zm-10.338 4.718l-7.58 3.46l-7.175-3.462l7.571-3.283zm-11.15 5.088l-8.197 3.74l-7.138-3.742l8.17-3.543zM23.192 70l-8.893 4.057l-7.07-4.058l8.843-3.835z' fill-rule='evenodd' fill='#000000'></path><path d='M18.492 24.736a1.5 1.5 0 0 0-.676.186a1.5 1.5 0 0 0-.586 2.039l20.282 36.633a1.5 1.5 0 0 0 2.037.586a1.5 1.5 0 0 0 .586-2.04l-20.28-36.632a1.5 1.5 0 0 0-1.363-.772z' fill-rule='evenodd' fill='#000000'></path><path d='M76.266 25.79a1.5 1.5 0 0 0-1.371.901L59.457 62.27a1.5 1.5 0 0 0 .78 1.974a1.5 1.5 0 0 0 1.972-.78l15.437-35.58a1.5 1.5 0 0 0-.779-1.972a1.5 1.5 0 0 0-.601-.123z' fill-rule='evenodd' fill='#000000'></path><path d='M31.246 13.873c-.096 0-.192.006-.287.02L5.754 17.338a2 2 0 0 0-1.674 1.521L.053 35.885a2 2 0 0 0 2.218 2.441l33.36-4.56a2 2 0 0 0 1.664-2.485l-4.129-15.91a2 2 0 0 0-1.92-1.498zm-1.506 4.223l3.117 12.011c-5.224-5.781-13.945-8.619-25.2-8.992z' fill='#000000'></path><path d='M60.2 14.441a2 2 0 0 0-1.962 2.4l3.826 18.655a2 2 0 0 0 1.721 1.584l33.977 4.092c1.698.204 2.852-1.679 1.9-3.1L86.406 18.285a2 2 0 0 0-1.422-.873l-24.548-2.957a2.005 2.005 0 0 0-.237-.014zm2.515 4.317l20.88 2.515c-7.47 1.148-13.759 4.538-17.902 12.006z' fill='#000000'></path></svg>",
+            "layers": [
+              {
+                "label": "Alfagia 01",
+                "type": "Cesium3DTileset",
+                "description": "",
+                "visible": false,
+                "moreInfoLink": "/view/urn%3Auuid%3A028790ad-4ebf-4870-85ac-e72ae9b8db26",
+                "downloadLink": "https://smithsonian.dataone.org/tiles/",
+                "attribution": "Smithsonian Tropical Research Institute",
+                "cesiumOptions": {
+                  "url": "https://smithsonian.dataone.org/tiles/3dtiles/01_alfagia_01/tileset.json"
+                }
+              },
+              {
+                "label": "Alfagia 02",
+                "type": "Cesium3DTileset",
+                "description": "",
+                "visible": false,
+                "moreInfoLink": "/view/urn%3Auuid%3A028790ad-4ebf-4870-85ac-e72ae9b8db26",
+                "downloadLink": "https://smithsonian.dataone.org/tiles/",
+                "attribution": "Smithsonian Tropical Research Institute",
+                "cesiumOptions": {
+                  "url": "https://smithsonian.dataone.org/tiles/3dtiles/02_alfagia_02/tileset.json"
+                }
+              },
+              {
+                "label": "Barro Colorado Island",
+                "type": "Cesium3DTileset",
+                "description": "",
+                "visible": false,
+                "moreInfoLink": "/view/urn%3Auuid%3A028790ad-4ebf-4870-85ac-e72ae9b8db26",
+                "downloadLink": "https://smithsonian.dataone.org/tiles/",
+                "attribution": "Smithsonian Tropical Research Institute",
+                "cesiumOptions": {
+                  "url": "https://smithsonian.dataone.org/tiles/3dtiles/03_barro_colorado_island/tileset.json"
+                }
+              },
+              {
+                "label": "Gigante",
+                "type": "Cesium3DTileset",
+                "description": "",
+                "visible": false,
+                "moreInfoLink": "/view/urn%3Auuid%3A028790ad-4ebf-4870-85ac-e72ae9b8db26",
+                "downloadLink": "https://smithsonian.dataone.org/tiles/",
+                "attribution": "Smithsonian Tropical Research Institute",
+                "cesiumOptions": {
+                  "url": "https://smithsonian.dataone.org/tiles/3dtiles/04_gigante/tileset.json"
+                }
+              },
+              {
+                "label": "Parque Metropolitano",
+                "type": "Cesium3DTileset",
+                "description": "",
+                "visible": false,
+                "moreInfoLink": "/view/urn%3Auuid%3A028790ad-4ebf-4870-85ac-e72ae9b8db26",
+                "downloadLink": "https://smithsonian.dataone.org/tiles/",
+                "attribution": "Smithsonian Tropical Research Institute",
+                "cesiumOptions": {
+                  "url": "https://smithsonian.dataone.org/tiles/3dtiles/05_parque_metropolitano/tileset.json"
+                }
+              },
+              {
+                "label": "Panama Pacifico",
+                "type": "Cesium3DTileset",
+                "description": "",
+                "visible": false,
+                "moreInfoLink": "/view/urn%3Auuid%3A028790ad-4ebf-4870-85ac-e72ae9b8db26",
+                "downloadLink": "https://smithsonian.dataone.org/tiles/",
+                "attribution": "Smithsonian Tropical Research Institute",
+                "cesiumOptions": {
+                  "url": "https://smithsonian.dataone.org/tiles/3dtiles/06_panama_pacifico/tileset.json"
+                }
+              },
+              {
+                "label": "San Lorenzo",
+                "type": "Cesium3DTileset",
+                "description": "",
+                "visible": false,
+                "moreInfoLink": "/view/urn%3Auuid%3A028790ad-4ebf-4870-85ac-e72ae9b8db26",
+                "downloadLink": "https://smithsonian.dataone.org/tiles/",
+                "attribution": "Smithsonian Tropical Research Institute",
+                "cesiumOptions": {
+                  "url": "https://smithsonian.dataone.org/tiles/3dtiles/07_san_lorenzo/tileset.json"
+                }
+              },
+              {
+                "label": "Santa Rita",
+                "type": "Cesium3DTileset",
+                "description": "",
+                "visible": false,
+                "moreInfoLink": "/view/urn%3Auuid%3A028790ad-4ebf-4870-85ac-e72ae9b8db26",
+                "downloadLink": "https://smithsonian.dataone.org/tiles/",
+                "attribution": "Smithsonian Tropical Research Institute",
+                "cesiumOptions": {
+                  "url": "https://smithsonian.dataone.org/tiles/3dtiles/08_santa_rita/tileset.json"
+                }
+              },
+              {
+                "label": "Soberania el Charco 01",
+                "type": "Cesium3DTileset",
+                "description": "",
+                "visible": false,
+                "moreInfoLink": "/view/urn%3Auuid%3A028790ad-4ebf-4870-85ac-e72ae9b8db26",
+                "downloadLink": "https://smithsonian.dataone.org/tiles/",
+                "attribution": "Smithsonian Tropical Research Institute",
+                "cesiumOptions": {
+                  "url": "https://smithsonian.dataone.org/tiles/3dtiles/09_soberania_el_charco/tileset.json"
+                }
+              },
+              {
+                "label": "Soberania el Charco 02",
+                "type": "Cesium3DTileset",
+                "description": "",
+                "visible": false,
+                "moreInfoLink": "/view/urn%3Auuid%3A028790ad-4ebf-4870-85ac-e72ae9b8db26",
+                "downloadLink": "https://smithsonian.dataone.org/tiles/",
+                "attribution": "Smithsonian Tropical Research Institute",
+                "cesiumOptions": {
+                  "url": "https://smithsonian.dataone.org/tiles/3dtiles/10_soberania_02/tileset.json"
+                }
+              }
+            ]
+          },
+          {
+            "label": "Base Layers",
+            "icon": "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 576 512'><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d='M160 32c-35.3 0-64 28.7-64 64V320c0 35.3 28.7 64 64 64H512c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H160zM396 138.7l96 144c4.9 7.4 5.4 16.8 1.2 24.6S480.9 320 472 320H328 280 200c-9.2 0-17.6-5.3-21.6-13.6s-2.9-18.2 2.9-25.4l64-80c4.6-5.7 11.4-9 18.7-9s14.2 3.3 18.7 9l17.3 21.6 56-84C360.5 132 368 128 376 128s15.5 4 20 10.7zM192 128a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zM48 120c0-13.3-10.7-24-24-24S0 106.7 0 120V344c0 75.1 60.9 136 136 136H456c13.3 0 24-10.7 24-24s-10.7-24-24-24H136c-48.6 0-88-39.4-88-88V120z'/></svg>",
+            "layers": [
+              {
+                "label": "Satellite imagery",
+                "icon": "urn:uuid:ff153eab-490b-46a4-b11e-47e0592735f6",
+                "type": "IonImageryProvider",
+                "description": "Global satellite imagery down to 15 cm resolution in urban areas",
+                "attribution": "Data provided by Bing Maps © 2021 Microsoft Corporation",
+                "moreInfoLink": "https://www.microsoft.com/maps",
+                "cesiumOptions": {
+                  "ionAssetId": "2"
+                }
+              }
+            ]
+          }
+        ],
+        "terrains": [
+          {
+            "label": "Cesium World Terrain",
+            "type": "CesiumTerrainProvider",
+            "cesiumOptions": {
+              "ionAssetId": "1",
+              "requestVertexNormals": true
+            }
+          }
+        ],
+        "showToolbar": true,
+        "toolbarOpen": true,
+        "showScaleBar": true,
+        "showFeatureInfo": true,
+        "showViewfinder": true
+    },
 
     /* Metadata View */
 

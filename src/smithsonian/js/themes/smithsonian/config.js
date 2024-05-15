58d9e8e0be916a193c9bb7e1af69b72fac2a4788
Ian Nesbitt <ian.nesbitt@gmail.com>
2024-05-15 13:13:38 -0700

    updating si search map

--- src/smithsonian/js/themes/smithsonian/config.js
+++ src/smithsonian/js/themes/smithsonian/config.js
@@ -167,7 +167,33 @@ MetacatUI.AppConfig = Object.assign({
             "icon": "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 576 512'><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d='M160 32c-35.3 0-64 28.7-64 64V320c0 35.3 28.7 64 64 64H512c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H160zM396 138.7l96 144c4.9 7.4 5.4 16.8 1.2 24.6S480.9 320 472 320H328 280 200c-9.2 0-17.6-5.3-21.6-13.6s-2.9-18.2 2.9-25.4l64-80c4.6-5.7 11.4-9 18.7-9s14.2 3.3 18.7 9l17.3 21.6 56-84C360.5 132 368 128 376 128s15.5 4 20 10.7zM192 128a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zM48 120c0-13.3-10.7-24-24-24S0 106.7 0 120V344c0 75.1 60.9 136 136 136H456c13.3 0 24-10.7 24-24s-10.7-24-24-24H136c-48.6 0-88-39.4-88-88V120z'/></svg>",
             "layers": [
               {
+                "label": "Open Topographic Map",
+                "layerId": "open-topographic-map",
+                "type": "OpenStreetMapImageryProvider",
+                "visible": false,
+                "description": "OpenTopoMap is a free topographic map generated from OpenStreetMap and SRTM elevation data. The map style is based on the official maps and relies on good readability through high contrast and balanced signatures.",
+                "attribution": "Stefan Erhardt",
+                "moreInfoLink": "https://opentopomap.org/credits",
+                "cesiumOptions": {
+                  "url": "https://a.tile.opentopomap.org/"
+                },
+                "opacity": 0.4,
+                "saturation": 0.1
+              },
+              {
+                "label": "Open Street Maps",
+                "layerId": "open-street-maps",
+                "type": "OpenStreetMapImageryProvider",
+                "visible": false,
+                "description": "OpenStreetMap is built by a community of mappers that contribute and maintain data about roads, trails, cafés, railway stations, and much more, all over the world.",
+                "attribution": "OpenStreetMap's community is diverse, passionate, and growing every day. Our contributors include enthusiast mappers, GIS professionals, engineers running the OSM servers, humanitarians mapping disaster-affected areas, and many more.",
+                "moreInfoLink": "https://www.openstreetmap.org/about",
+                "opacity": 0.6,
+                "saturation": 0.1
+              },
+              {
                 "label": "Satellite imagery",
+                "layerId": "satellite-imagery",
                 "icon": "urn:uuid:ff153eab-490b-46a4-b11e-47e0592735f6",
                 "type": "IonImageryProvider",
                 "description": "Global satellite imagery down to 15 cm resolution in urban areas",

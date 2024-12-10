8eae12f8e897bd5cb1c128fa197e54eb9b672f6c
Ian Nesbitt <ian.nesbitt@gmail.com>
2024-12-10 06:49:00 -0800

    adding CIB theme

---
+++ src/cib/js/themes/cib/config.js
@@ -0,0 +1,74 @@
+MetacatUI.AppConfig = Object.assign({
+    root: "/",
+    theme: "cib",
+    title: "California Institute for Biodiversity",
+    repositoryName: "California Institute for Biodiversity",
+    repositoryURL: "https://cib.dataone.org",
+    nodeId: "urn:node:CIB",
+    emailContact: "support@dataone.org",
+    description: "The California Institute for Biodiversity (CIB) Data Repository is intended to preserve and share critical biodiversity data collected by CIB grantees and to support efforts to understand and sustain the great diversity of life in California.",
+
+    /* Metadata View */
+
+    /* Metrics */
+    displayDatasetQualityMetric: true,
+    hideSummaryMetadataAssessment: false,
+    hideSummaryDownloadsChart: false,
+    hideSummaryCitationsChart: false,
+    hideSummaryViewsChart: false,
+
+    /* Editor */
+    defaultAccessPolicy: [
+        {
+            subject: "CN=DataONE-Support,DC=dataone,DC=org",
+            read: true,
+            write: true,
+            changePermission: true
+        },
+        {
+            subject: "public",
+            read: true
+        }
+    ],
+    hiddenSubjectsInAccessPolicy: ["CN=DataONE-Support,DC=dataone,DC=org"],
+    emlEditorRequiredFields: {
+        abstract: true,
+        alternateIdentifier: false,
+        funding: false,
+        generalTaxonomicCoverage: false,
+        geoCoverage: true,
+        intellectualRights: true,
+        keywordSets: false,
+        methods: false,
+        samplingDescription: false,
+        studyExtentDescription: false,
+        taxonCoverage: false,
+        temporalCoverage: true,
+        title: true
+    },
+    enableSolrJoins: true
+}, (MetacatUI.AppConfig || {}));
+
+MetacatUI.themeMap =
+{
+    "*": {
+        // Routers
+        "routers/BaseRouter": MetacatUI.root + "/js/routers/router.js",
+        "routers/router": MetacatUI.root + "/js/themes/" + MetacatUI.theme + "/routers/router.js",
+        // Models
+        "models/BaseMap": MetacatUI.root + "/js/models/Map.js",
+        "models/Map": MetacatUI.root + "/js/themes/" + MetacatUI.theme + "/models/Map.js",
+        // Views
+        "views/metadata/BaseEML211View": MetacatUI.root + "/js/views/metadata/EML211View.js",
+        "views/metadata/EML211View": MetacatUI.root + "/js/themes/" + MetacatUI.theme + "/views/metadata/EML211View.js",
+        "views/BaseTextView": MetacatUI.root + "/js/views/TextView.js",
+        "views/TextView": MetacatUI.root + "/js/themes/" + MetacatUI.theme + "/views/TextView.js",
+        // Templates
+        "templates/navbar.html": MetacatUI.root + "/js/themes/" + MetacatUI.theme + "/templates/navbar.html",
+        "templates/about.html": MetacatUI.root + "/js/themes/" + MetacatUI.theme + "/templates/about.html",
+        "templates/support.html": MetacatUI.root + "/js/themes/" + MetacatUI.theme + "/templates/support.html",
+        "templates/metadata/metadataOverview.html": MetacatUI.root + "/js/themes/" + MetacatUI.theme + "/templates/metadata/metadataOverview.html",
+        "templates/footer.html": MetacatUI.root + "/js/themes/" + MetacatUI.theme + "/templates/footer.html",
+        "templates/mainContent.html": MetacatUI.root + '/js/themes/' + MetacatUI.theme + '/templates/mainContent.html'
+    }
+}

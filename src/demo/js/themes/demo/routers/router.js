19893fa63fd5d6d393ed83deafc9b4fe6c083604
Ian Nesbitt <ian.nesbitt@gmail.com>
2026-04-30 14:00:44 -0700

    scratching GOA theme copy for fresh boilerplate for demo

--- src/demo/js/themes/demo/routers/router.js
+++ src/demo/js/themes/demo/routers/router.js
@@ -1,19 +1,30 @@
 /*global Backbone */
-"use strict";
+'use strict';
 
-define(["jquery", "underscore", "backbone", "routers/BaseRouter"], function (
-  $,
-  _,
-  Backbone,
-  BaseRouter,
-) {
-  // MetacatUI Router
-  // ----------------
-  var UIRouter = BaseRouter.extend({
-    // Don't allow editing or submitting for this node
-    renderEditor: function () {
-      this.renderIndex();
-    },
-  });
-  return UIRouter;
+define(['jquery', 'underscore', 'backbone', 'routers/BaseRouter'],
+function ($, _, Backbone, BaseRouter) {
+
+    // MetacatUI Router
+    // ----------------
+    var UIRouter = BaseRouter.extend({
+        routes: _.extend(BaseRouter.prototype.routes, {
+            "support(/:anchorId)(/)": "renderSupport",
+        }),
+        
+        /**
+         * Render the support page
+         * @return {object} the rendered support page
+         */
+        renderSupport: function(anchorId) {
+            this.routeHistory.push("support");
+            MetacatUI.appModel.set('anchorId', anchorId);
+            var options = {
+                    pageName: "support",
+                    anchorId: anchorId
+                }
+
+            this.renderText(options);
+        }
+    });
+    return UIRouter;
 });
